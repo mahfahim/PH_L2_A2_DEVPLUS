f0a0c75a7d1a3696259f8e74796b8e545545a0a2
Md. Abdul Hai Fahim <mahfahim51@gmail.com>
2026-05-24 16:02:44 +0600

    issues.router is updated

--- src/modules/issues/issues.route.ts
+++ src/modules/issues/issues.route.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.get("/", issuesController.getAllIssues);
 router.get("/:id", issuesController.getSingleIssue);
 router.post("/", authMiddleware(["contributor", "maintainer"]), issuesController.createIssue);
-router.patch("/:id", authMiddleware(["contributor", "maintainer"]), issuesController.updateIssue);
+router.patch("/:id", authMiddleware([ "maintainer"]), issuesController.updateIssue);
 router.delete("/:id", authMiddleware(["maintainer"]), issuesController.deleteIssue);
 
 export const issuesRoute = router;
